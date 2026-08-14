445415c0fdbf6f8d40efdd4e873063ef1bd5ca62
Nazareno <nazarenosiciliano22@gmail.com>
2026-08-13 23:18:33 -0300

    Añadir rutas de usuarios y login funcional

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
-app.use('/api', require('./routes/usuarios'));
+
 
 // Sistema de Logs de errores
 const logStream = fs.createWriteStream(path.join(__dirname, 'errores.log'), { flags: 'a' });
 console.error = (msg) => logStream.write(`${new Date().toISOString()} - ERROR: ${msg}\n`);
 
 // Rutas modulares (Conectamos los archivos de la carpeta routes)
+app.use('/api', require('./routes/usuarios')); // Manejará /api/login y /api/register
 app.use('/api/productos', require('./routes/productos'));
 app.use('/api/agenda', require('./routes/agenda'));
 // (Acá podés seguir sumando rutas limpiamente a futuro, ej: app.use('/api/recetas', require('./routes/recetas')));
@@ -26,4 +27,3 @@ app.use('/api/agenda', require('./routes/agenda'));
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
-    
