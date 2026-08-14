2d1e924af81ea3b35a6fc5b42d68a64f9041546e
Nazareno <nazarenosiciliano22@gmail.com>
2026-08-13 23:08:13 -0300

    Agregar configuración SSL para la base de datos en Aiven

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // <--- Esto es lo que permite que Aiven acepte el tráfico seguro desde Render
+    }
 });
 
 module.exports = pool.promise();
